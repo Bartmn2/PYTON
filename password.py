290549d017d29ea17b6bc55da04c1e02e9b27440
bartmn3@gmail.com <bartmn3@gmail.com>
2022-10-07 17:51:51 +0300

    PYTON

---
+++ password.py
@@ -0,0 +1,14 @@
+import random
+digits = '0123456789'
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+punctuation = '!#$%&*+-=?@^_.'
+def chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&*+-=?@^_.'
+
+cntPw = input('Укажите количество паролей для генерации:')
+lenPw = input('Укажите длину одного пароля:')
+digOn = input('Включать ли цифры 0123456789? (y/n)')
+ABCon = input('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? (y/n)')
+abcOn = input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? (y/n)')
+chOn = input('Включать ли символы !#$%&*+-=?@^_? (y/n)')
+excOn = input('Исключать ли неоднозначные символы il1Lo0O? (y/n)')
